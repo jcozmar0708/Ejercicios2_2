fd10fe5db9ddca0ee7aeac50d1c1054af9b5e076
jcozmar0708 <jcozmar0708@g.educaand.es>
2023-10-25 09:55:06 +0200

    media estadistica

--- src/AdivinaNumero.java
+++ src/AdivinaNumero.java
@@ -7,15 +7,15 @@ public class AdivinaNumero {
         int numero_random = (int) (Math.random()*100 + 1);
         int n;
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 10; i > 0; i--) {
             System.out.println("Escribe un número:");
             n = sc.nextInt();
             if (n == numero_random) {
                 System.out.println("Has acertado");
                 break;
             } else {
-                if (i != 9) System.out.println("Has fallado el número es " + (n>numero_random ? "menor" : "mayor"));
-                else System.out.println("Has fallado");
+                if (i != 1) System.out.printf("Has fallado el número es %s quedan %d intentos\n",(n>numero_random ? "menor" : "mayor"),i-1);
+                else System.out.printf("Has fallado quedan %d intentos\n", i);
             }
         }
         System.out.println("El número era: " + numero_random);
